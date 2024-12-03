7df7debf7ed6b657fd87f136cef0efbdc14381a7
aetherk13 <kedar.krishnan29@gmail.com>
2024-12-02 23:11:38 -0500

    Added Encoder and BBA modules

---
+++ EncoderModule.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+class MobileNetV2Encoder(nn.Module):
+    def __init__(self, pretrained=True):
+        super(MobileNetV2Encoder, self).__init__()
+
+        # Load the MobileNetV2 model
+        mobilenet_v2 = models.mobilenet_v2(pretrained=pretrained)
+        
+        # Extract the feature layers
+        self.features = mobilenet_v2.features
+        
+        self.map1 = nn.Sequential(*self.features[:2])  #1/4 res
+        self.map2 = nn.Sequential(*self.features[2:4]) #1/8 res
+        self.map3 = nn.Sequential(*self.features[4:7]) #1/16 res
+        self.map4 = nn.Sequential(*self.features[7:])  #1/32 res
+
+    def forward(self, x):
+        x1 = self.map1(x) 
+        x2 = self.map2(x1)
+        x3 = self.map3(x2)
+        x4 = self.map4(x3)
+        
+        return x1, x2, x3, x4
